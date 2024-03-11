5918e839ba629416b125cab58fc3c66da879609f
danishshaikh9702 <danish.shaikh@respo.co.in>
2024-03-11 16:56:01 +0530

    h

--- src/implements/cacheimpl.ts
+++ src/implements/cacheimpl.ts
@@ -1,9 +1,9 @@
-import { NameSpaceConstants } from "..";
+import { NameSpaceEnums } from "..";
 import { StandardResponse, valueType } from "../types";
 // not required
 interface CacheImpl{
-     set(namespace:NameSpaceConstants, key:string, value:valueType):Promise<StandardResponse>
-     get(namespace:NameSpaceConstants, key:string):Promise<StandardResponse>
-     delete(namespace:NameSpaceConstants, key:string):Promise<StandardResponse>
+     set(namespace:NameSpaceEnums, key:string, value:valueType):Promise<StandardResponse>
+     get(namespace:NameSpaceEnums, key:string):Promise<StandardResponse>
+     delete(namespace:NameSpaceEnums, key:string):Promise<StandardResponse>
 }
 export default CacheImpl
